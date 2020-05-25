abb0dedbad238533db889a30406633fc6e937773
steveDro <akotosteven@gmail.com>
2020-05-25 18:59:48 +0000

    changes inline code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -56,7 +56,7 @@ class Counter extends Component {
             <button
               onClick={() => this.props.onDecrement(this.props.counter)}
               className="btn btn-secondary btn-sm"
-              disabled={this.disabled()}
+              disabled={this.props.counter.value === 0}
             >
               -
             </button>
